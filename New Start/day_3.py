d4dd9999c42d7d0508246bc181abdd717fdfe5e6
ryan-tensen <kumaransengu@gmail.com>
2026-06-04 21:53:41 +0530

    day_3: Functions

--- New Start/day_3.py
+++ New Start/day_3.py
@@ -1,20 +1,20 @@
-# def get_big_number(num1, num2):
-#     if num1 > num2:
-#         return num1
-#     else:
-#         return num2
-#
-# print(get_big_number(1, 2))
-#
-#
-# def sum_of_list(numbers):
-#     num = 0
-#     for i in numbers:
-#         num += i
-#
-#     return num
-#
-# print(sum_of_list([1,2,3,4,5]))
+def get_big_number(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+print(get_big_number(1, 2))
+
+
+def sum_of_list(numbers):
+    num = 0
+    for i in numbers:
+        num += i
+
+    return num
+
+print(sum_of_list([1,2,3,4,5]))
 
 def inverse_of_string(word):
     inverse = ""
@@ -25,26 +25,26 @@ def inverse_of_string(word):
 print(inverse_of_string("kumaresan"))
 
 
-# def palindrome_check(word):
-#     inverse = ""
-#     for i in word:
-#         inverse = i + inverse
-#
-#     if inverse == word:
-#         return True
-#     else:
-#         return False
-#
-# print(palindrome_check(("racecar")))
-#
-# def palindrome_check(word):
-#     inverse = inverse_of_string(word)
-#     if inverse == word:
-#         return True
-#     else:
-#         return False
-#
-# print(palindrome_check(("racecar")))
+def palindrome_check(word):
+    inverse = ""
+    for i in word:
+        inverse = i + inverse
+
+    if inverse == word:
+        return True
+    else:
+        return False
+
+print(palindrome_check(("racecar")))
+
+def palindrome_check(word):
+    inverse = inverse_of_string(word)
+    if inverse == word:
+        return True
+    else:
+        return False
+
+print(palindrome_check(("racecar")))
 
 def palindrome_check(word):
     inverse = inverse_of_string(word)
