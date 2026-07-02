1ed212794e02686e60dbcf476a5b801fdef3881b
KumaresanSengamuthu <kumaresan.s@vsolv.co.in>
2026-07-02 07:22:10 +0530

    DSA day 2: two sum hashmap find duplicates

--- New Start/two_sum.py
+++ New Start/two_sum.py
@@ -16,3 +16,40 @@ for index, i in enumerate(nums):
         break
 
 print(result)
+
+
+# using dictionary method:
+
+def two_sum(nums, target):
+    seen = {}
+    for index , num in enumerate(nums):
+        need = target - num
+        if need in seen:
+            return [seen[need],index]
+        seen[num] = index
+
+print(two_sum(nums, 9))
+
+
+# Given an array, find if there are any duplicates.
+# Return True if any value appears more than once.
+# Return False if all values are distinct.
+
+# Example 1:
+# nums = [1, 2, 3, 4]
+# Output: False
+
+# Example 2:
+# nums = [1, 2, 3, 1]
+# Output: True
+
+def find_distinct(nums):
+    seen = set()
+    for i in nums:
+        if i in seen:
+            return True
+        seen.add(i)
+    return False
+
+print(find_distinct([1, 2, 3, 1]))
+print(find_distinct([1, 2, 3, 4]))
