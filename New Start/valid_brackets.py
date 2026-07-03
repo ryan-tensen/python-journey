3c6b01bf7eb367aa59c571b7973a86a68fc03cca
KumaresanSengamuthu <kumaresan.s@vsolv.co.in>
2026-07-03 07:29:38 +0530

    DSA day 3 night task : min max

--- New Start/valid_brackets.py
+++ New Start/valid_brackets.py
@@ -52,3 +52,26 @@ print(valid_brackets("()[]{}"))  # True
 print(valid_brackets("(]"))      # False
 print(valid_brackets("{[]}"))    # True
 print(valid_brackets("([)]"))
+
+# Find max and min without built-in functions:
+#
+# nums = [3, 1, 4, 1, 5, 9, 2, 6]
+# Output: Max = 9, Min = 1
+#
+# Rules:
+# → No max(), no min()
+# → Use a loop
+# → Think: how would you track the largest/smallest
+#   number as you loop through?
+
+def min_max(nums):
+    min = nums[0]
+    max = nums[0]
+    for i in nums:
+        if i >= max :
+            max = i
+        if i <= min:
+            min = i
+    return f"Max = {max}, min = {min}"
+
+print(min_max([3, 1, 4, 1, 5, 9, 2, 6]))
